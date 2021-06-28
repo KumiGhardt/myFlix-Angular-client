a8b39ba09b7dc3959c3b020031389c096563dbb0
Kumi <64310263+KumiGhardt@users.noreply.github.com>
2021-06-28 22:51:33 +0200

    this commit adds the user registration component

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -3,11 +3,11 @@ import { Component, OnInit, Input } from '@angular/core';
 // You'll use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
 
-// This import brings in the API calls we created in 6.2
-import { UserRegistrationService } from './fetch-api-data.service';
-
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
+
+//brings in the API calls 
+import { UserRegistrationService } from '../fetch-api-data.service';
 
 
 @Component({
@@ -18,28 +18,27 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class UserRegistrationFormComponent implements OnInit {
 
   @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
-
-constructor(
+  
+  constructor(
     public fetchApiData: UserRegistrationService,
     public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
     public snackBar: MatSnackBar) { }
 
-ngOnInit(): void {
-}
+  ngOnInit(): void {
+  }
 
-// This is the function responsible for sending the form inputs to the backend
-registerUser(): void {
+  // This is the function responsible for sending the form inputs to the backend
+  registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
   // Logic for a successful user registration goes here! (To be implemented)
-     this.dialogRef.close(); // This will close the modal on success!
-     this.snackBar.open(result, 'OK', {
+    this.dialogRef.close(); // This will close the modal on success!
+      this.snackBar.open(result, 'OK', {
         duration: 2000
-     });
+      });
     }, (result) => {
       this.snackBar.open(result, 'OK', {
         duration: 2000
       });
     });
   }
-
-  }
+}
